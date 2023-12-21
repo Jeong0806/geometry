bd6c7f5298a600119521ac3f29b7cdb64eb96612
Jeong0806 <Jeong0806@github.com>
2023-12-22 00:45:26 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author Jeong Seong In (0806jsi@gmail.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-22
+ * @copyright Copyright (c) 2023 Jeong0806, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 Jeong0806, All Rights Reserved.
+// Author Jeong Seong In (0806jsi@gmail.com)
+
+#ifndef Jeong0806_GEOMETRY_POINT_2D_HPP_
+#define Jeong0806_GEOMETRY_POINT_2D_HPP_
+
+#endif  // Jeong0806_GEOMETRY_POINT_2D_HPP_
