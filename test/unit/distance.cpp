1997481ed2e8b0a8efa295821e216731070c8da6
Jeong0806 <Jeong0806@github.com>
2023-12-27 21:28:52 +0900

    Added test case

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -5,10 +5,16 @@
 
 #include "gtest/gtest.h"
 
-namespace Jeong0806::geometry{
+namespace Jeong0806::geometry {
   TEST(GeometryDistance, Constructor){
     Distance distance1;
     Distance distance2(distance1);
     Distance distance3(std::move(Distance()));
   }
+
+  TEST(GeometryDistance, AssignmentOperator) {
+  Distance distance1;
+  auto distance2 = distance1;
+  auto distance3 = std::move(Distance());
+}
 }
