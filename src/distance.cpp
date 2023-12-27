58eee33f73cbdf6c2db68bcdce4d5294398c35e6
Jeong0806 <Jeong0806@github.com>
2023-12-27 22:08:58 +0900

    Added the implementation of the constructor with values

--- src/distance.cpp
+++ src/distance.cpp
@@ -12,4 +12,40 @@
 
 #include "geometry/distance.hpp"
 
-namespace Jeong0806::geometry {}
+#include <cstdint>
+
+namespace {
+auto ScaleDistanceToNanometer(double value,
+                              Jeong0806::geometry::Distance::DistanceType type)
+    -> int64_t {
+  int64_t result;
+  switch (type) {
+    case Jeong0806::geometry::Distance::DistanceType::kKilometer:
+      result = static_cast<int64_t>(value * 1.0e+12);
+      break;
+    case Jeong0806::geometry::Distance::DistanceType::kMeter:
+      result = static_cast<int64_t>(value * 1.0e+9);
+      break;
+    case Jeong0806::geometry::Distance::DistanceType::kCentimeter:
+      result = static_cast<int64_t>(value * 1.0e+7);
+      break;
+    case Jeong0806::geometry::Distance::DistanceType::kMillimeter:
+      result = static_cast<int64_t>(value * 1.0e+6);
+      break;
+    case Jeong0806::geometry::Distance::DistanceType::kMicrometer:
+      result = static_cast<int64_t>(value * 1.0e+3);
+      break;
+    case Jeong0806::geometry::Distance::DistanceType::kNanometer:
+      result = static_cast<int64_t>(value);
+      break;
+    default: result = static_cast<int64_t>(value * 1.0e+9);
+      break;
+  }
+  return result;
+}
+}  // namespace
+
+namespace Jeong0806::geometry {
+Distance::Distance(double value, DistanceType type)
+    : nanometer_(ScaleDistanceToNanometer(value, type)) {}
+}  // namespace Jeong0806::geometry
