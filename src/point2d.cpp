2b1985b83aaad55e392547b73fcf797c1f4fcaf9
Jeong0806 <Jeong0806@github.com>
2023-12-27 12:16:26 +0900

    Added point2d constructor with coordinate values

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -1,2 +1,17 @@
+/**
+ * @file geometry/src/point2d.cpp
+ * @author Jeong Seong In (0806jsi@gmail.com)
+ * @brief Point class developments with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-27
+ * @copyright Copyright (c) 2023 Jeong0806, All Rights Reserved.
+ */
+
 // Copyright (c) 2023 Jeong0806, All Rights Reserved.
 // Author Jeong Seong In (0806jsi@gmail.com)
+
+#include "geometry/point2d.hpp"
+
+namespace Jeong0806::geometry {
+Point2D::Point2D(double x, double y) : x_(x), y_(y) {}
+}  // namespace Jeong0806::geometry
