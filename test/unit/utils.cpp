2b71103ea7d3a001b30b302444220ebb41a73a65
Jeong0806 <Jeong0806@github.com>
2023-12-27 09:47:40 +0900

    Added skeloton codes

---
+++ test/unit/utils.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Jeong0806, All Rights Reserved.
+// Author Jeong Seong In (0806jsi@gmail.com)
+
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <limits>
+#include <stdexcept>
+
+#include "geometry/point2d.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+constexpr uint32_t kTestCount = 1000U;
+}
+
+namespace jeong0806::geometry {}  // namespace jeong0806::geometry
