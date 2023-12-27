a743d3ccfc1f4f54729e28658b572145564591ad
Jeong0806 <Jeong0806@github.com>
2023-12-27 10:35:59 +0900

    Added test case for constructor

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -1,17 +1,23 @@
 // Copyright (c) 2023 Jeong0806, All Rights Reserved.
 // Author Jeong Seong In (0806jsi@gmail.com)
 
+#include "geometry/point2d.hpp"
+
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
 #include <limits>
 #include <stdexcept>
 
-#include "geometry/point2d.hpp"
 #include "gtest/gtest.h"
 
 namespace {
 constexpr uint32_t kTestCount = 1000U;
 }
 
-namespace jeong0806::geometry {}  // namespace jeong0806::geometry
+namespace Jeong0806::geometry {
+TEST(GeometryPoint2D, Constructor) {
+  Point2D point1;
+  Point2D point2(0.0,0.0);
+}
+}  // namespace jeong0806::geometry
