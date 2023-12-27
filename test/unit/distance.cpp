6fea2a38eb3aa3399bf2b7da562f35e21c8a4bea
Jeong0806 <Jeong0806@github.com>
2023-12-27 21:43:50 +0900

    Added test case for constructor with value

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -3,18 +3,42 @@
 
 #include "geometry/distance.hpp"
 
+#include <cstdint>
+
 #include "gtest/gtest.h"
 
-namespace Jeong0806::geometry {
-  TEST(GeometryDistance, Constructor){
-    Distance distance1;
-    Distance distance2(distance1);
-    Distance distance3(std::move(Distance()));
-  }
+namespace {
+constexpr uint32_t kTestCount = 1000U;
+}
 
-  TEST(GeometryDistance, AssignmentOperator) {
+namespace Jeong0806::geometry {
+TEST(GeometryDistance, Constructor) {
+  Distance distance1;
+  Distance distance2(distance1);
+  Distance distance3(std::move(Distance()));
+}
+
+TEST(GeometryDistance, ConstructorWithValue) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    Distance distance1(static_cast<double>(std::rand()));
+    Distance distance2(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kKilometer);
+    Distance distance3(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kMeter);
+    Distance distance4(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kCentimeter);
+    Distance distance5(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kMillimeter);
+    Distance distance6(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kMicrometer);
+    Distance distance7(static_cast<double>(std::rand()),
+                       Distance::DistanceType::kNanometer);
+  }
+}
+
+TEST(GeometryDistance, AssignmentOperator) {
   Distance distance1;
   auto distance2 = distance1;
   auto distance3 = std::move(Distance());
 }
-}
+}  // namespace Jeong0806::geometry
