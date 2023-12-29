f2f215954131cb06e0151a4739daeda85a9eca06
Jeong0806 <Jeong0806@github.com>
2023-12-29 20:56:33 +0900

    Added the implementation of operator-=

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -46,4 +46,9 @@ auto Point2D::operator+=(const Point2D& other) -> void{
   x_ += other.x_;
   y_ += other.y_;
 }
+
+auto Point2D::operator-=(const Point2D& other) -> void{
+  x_ -= other.x_;
+  y_ -= other.y_;
+}
 }  // namespace Jeong0806::geometry
