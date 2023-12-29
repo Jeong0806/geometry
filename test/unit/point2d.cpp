0ccf02ec5c74979469fb0c52c6d2d74e1d617a2e
Jeong0806 <Jeong0806@github.com>
2023-12-29 18:54:38 +0900

    Added test case for operator-

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -116,4 +116,18 @@ TEST(GeometryPoint2D, operatorAdd) {
   }
 }
 
+TEST(GeometryPoint2D, operatorSubtract) {
+  for (uint32_t i; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+    const auto kTargetX = static_cast<double>(std::rand());
+    const auto kTargetY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+    Point2D target(kTargetX, kTargetY);
+
+    const auto point = source - target;
+  }
+}
+
 }  // namespace Jeong0806::geometry
